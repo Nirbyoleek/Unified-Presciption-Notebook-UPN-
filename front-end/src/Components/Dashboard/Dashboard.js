1d921b7dc934d0157337898a32804df8c9a3ddef
Nirbyoleek Das <nirbyoleekd@trakiot.in>
2022-04-02 18:07:22 +0530

    Signup Added

--- front-end/src/Components/Dashboard/Dashboard.js
+++ front-end/src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,46 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import Calendar from "../../assets/calendar.svg";
+import Clock from "../../assets/clock.svg";
+import Search from "../../assets/search.svg";
+
 function Dashboard() {
   return (
-    <div>
+    <div className="flex">
       <Sidebar />
+      <div className="bg-[#E5FFFE] w-[83.5vw] px-8 py-5">
+        <div className="flex justify-between mb-6">
+          <div class="relative px-4 py-2 bg-[#499189]  rounded-2xl flex justify-center w-[25vw]">
+            <img src={Search} alt="search" className="h-5 mr-2" />
+            <input
+              type="text"
+              name="name"
+              className="block w-full appearance-none rounded-xl text-white bg-[#499189] focus:outline-none"
+            />
+          </div>
+
+          <div>User</div>
+        </div>
+        <div className="flex">
+          <div className="flex mx-4">
+            <img src={Calendar} alt="calendar" className="w-5 mr-3" />
+            <h3>2nd April 2022</h3>
+          </div>
+          <div className="flex">
+            <img src={Clock} alt="clock" className="w-5 mr-3" />
+            <h3>10 : 10 PM</h3>
+          </div>
+        </div>
+        <div className="mt-20 bg-[#75c6bc7e] px-12 py-6 rounded-xl">
+          <div className="flex mb-4">
+            <div className="mr-4">Icon</div>
+            <div>
+              <p className="text-[#499189] font-bold text-xl">Dr. John Doe</p>
+              <p className="text-[#499189] font-bold text-sm">M.B.B.S</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- front-end/src/Components/Signup.js
+++ front-end/src/Components/Signup.js
@@ -1,6 +1,9 @@
 import React from "react";
 import SignUp from "../assets/signuppatient.svg";
 import logo from "../assets/logo1.svg";
+import email from "../assets/icons/email.svg";
+import user from "../assets/icons/user.svg";
+import password from "../assets/icons/password.svg";
 
 const Signup = () => {
   return (
@@ -11,13 +14,14 @@ const Signup = () => {
             <img src={logo} className="w-[7vw] h-[7vh]" />
           </div>
 
-          <div className="p-24">
-            <div class="relative border-b-2 p-2 focus-within:border-[#23776E]">
+          <div className="px-40 pt-24">
+            <div class="relative border-b-2 p-2 focus-within:border-[#23776E] flex">
               <input
                 type="text"
                 name="name"
                 placeholder=" "
                 class="block w-full appearance-none focus:outline-none  bg-transparent"
+                autoComplete="false"
               />
               <label
                 for="name"
@@ -25,57 +29,84 @@ const Signup = () => {
               >
                 Name
               </label>
+              <img src={user} />
             </div>
+          </div>
 
-            <div class="relative border-b-2 p-2 focus-within:border-[#23776E]">
+          <div className="px-40 pt-12 ">
+            <div class="relative border-b-2 p-2 focus-within:border-[#23776E] flex">
               <input
                 type="text"
                 name="name"
                 placeholder=" "
                 class="block w-full appearance-none focus:outline-none  bg-transparent"
+                autoComplete="false"
               />
               <label
                 for="name"
                 class="absolute top-0 -z-1 text-[#B0B0B0] duration-300 origin-0 p-2"
               >
-                Username
+                E-Mail
               </label>
+              <img src={email} />
             </div>
+          </div>
 
-            <div class="relative border-b-2 p-2 focus-within:border-[#23776E]">
+          <div className="px-40 pt-12 ">
+            <div class="relative border-b-2 p-2 focus-within:border-[#23776E] flex">
               <input
                 type="text"
-                name="username"
+                name="name"
                 placeholder=" "
                 class="block w-full appearance-none focus:outline-none  bg-transparent"
+                autoComplete="false"
               />
               <label
-                for="username"
+                for="name"
                 class="absolute top-0 -z-1 text-[#B0B0B0] duration-300 origin-0 p-2"
               >
-                Username
+                Password
               </label>
+              <img src={password} />
             </div>
+          </div>
 
-            <div class="relative border-b-2 p-2 focus-within:border-[#23776E]">
+          <div className="px-40 pt-12 ">
+            <div class="relative border-b-2 p-2 focus-within:border-[#23776E] flex">
               <input
                 type="text"
-                name="username"
+                name="name"
                 placeholder=" "
                 class="block w-full appearance-none focus:outline-none  bg-transparent"
+                autoComplete="false"
               />
               <label
-                for="username"
+                for="name"
                 class="absolute top-0 -z-1 text-[#B0B0B0] duration-300 origin-0 p-2"
               >
-                Username
+                Confirm Password
               </label>
+              <img src={password} />
+            </div>
+          </div>
+
+          <div className="px-40 pt-12 ">
+            <button className="bg-[#23776E] rounded-sm">
+              <p className="text-white py-4 w-[30vw]">Sign Up</p>
+            </button>
+            <div className="justify-center mt-4 text-sm flex">
+              <p className="mr-1">Already have an account? </p>
+              <strong>
+                <a href="/login" className="text-[#23776E]">
+                  Login
+                </a>
+              </strong>
             </div>
           </div>
         </div>
 
         <div className="w-[50vw] p-4">
-          <img src={SignUp} className=" w-200 h-[95vh]  ml-auto" />
+          <img src={SignUp} className=" w-200 h-[95vh]  mx-auto" />
         </div>
       </div>
     </div>
